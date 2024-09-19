b1c48d440759bf9b7b239aa2544c52642e753582
rishabh <aaarentalllp@Rishabh-MacBook-Pro.local>
2024-09-20 00:35:37 +0530

    changes

--- src/components/magicui/icon-cloud.tsx
+++ src/components/magicui/icon-cloud.tsx
@@ -109,7 +109,7 @@ export default function IconCloud({ iconSlugs, heroData }: DynamicCloudProps) {
             alignItems: "center",
           }}
         >
-          <Avatar className="size-40 border">
+          <Avatar className="size-32 border">
             <AvatarImage alt={heroData.name} src={heroData.avatarUrl} />
           </Avatar>
         </div>
